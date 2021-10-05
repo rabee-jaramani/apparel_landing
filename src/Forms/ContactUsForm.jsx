5dc3d1d74042cec435ae90a016fa6c27571f2fd9
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-10-05 10:41:28 +0400

    validation

--- src/Forms/ContactUsForm.jsx
+++ src/Forms/ContactUsForm.jsx
@@ -1,41 +1,103 @@
+import { useFormik } from "formik";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import * as Yup from "yup";
+import emailjs, { init } from "emailjs-com";
+import gsap from "gsap/all";
+
 
 const ContactUsForm = () => {
+  init("user_WXMrG9H2675m2AFa27nY0");
   const {t}=useTranslation()
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      phone: "",
+      message: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      phone: Yup.number()
+      .max(99999999999999, "Must be 15 numbers or less")
+      .min(111111111, "Must be 8 numbers or more")
+      .required("Required"),
+      message: Yup.string()
+        .max(300, "Must be 300 characters or less")
+        .min(5, "Must be 5 characters or more")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {
+      var templateParams = {
+        email: values.email,
+        phone:values.phone,
+        message: values.message,
+      };
+      gsap.to(".form-btn", {
+        opacity: 0.5,
+        pointerEvents: "none",
+        duration: 0.1,
+      });
+      emailjs.send("service_ebc0b89", "template_i9k1rca", templateParams).then(
+        (result) => {
+          console.log("this is the response", result.text);
+          alert("Thank you for your message 😊");
+          gsap.to(".form-btn", { opacity: 1, pointerEvents: "all" });
+        },
+        (error) => {
+          console.log("this is error", error.text);
+        }
+      );
+    },
+  });
   return (
-    <form className="form-flex">
+    <form className="form-flex" onSubmit={formik.handleSubmit}>
       <div className="form-item">
         <label htmlFor="name" className='text'>{t("Email Address")}</label>
         <br />
         <input
-          required
+           onChange={formik.handleChange}
+           onBlur={formik.handleBlur}
+           value={formik.values.email}
           id="email"
           type="email"
           placeholder="Mark@gmail.com"
         ></input>
+                  {formik.touched.email && formik.errors.email ? (
+            <div className="error-message">{formik.errors.email}</div>
+          ) : null}
       </div>
       <div className="form-item">
-        <label htmlFor="name" className='text'>{t("Phone")}</label>
+        <label htmlFor="phone" className='text'>{t("Phone")}</label>
         <br />
         <input
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.phone}
           type="tel"
-          required
-          pattern="[0-9]{10}"
-          id="contact-number"
+          id="phone"
           placeholder="051212***"
         ></input>
+                  {formik.touched.phone && formik.errors.phone ? (
+            <div className="error-message">{formik.errors.phone}</div>
+          ) : null}
       </div>
 
       <div className="form-item">
         <label htmlFor="message" className='text'>{t("Message")}</label>
         <br />
         <textarea
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.message}
           id="message"
           name="message"
           placeholder="Hello..."
           rows="5"
         ></textarea>
+                  {formik.touched.message && formik.errors.message ? (
+            <div className="error-message">{formik.errors.message}</div>
+          ) : null}
       </div>
 
       <div></div>
--- src/Forms/SubscribeForm.jsx
+++ src/Forms/SubscribeForm.jsx
@@ -1,38 +1,101 @@
+import gsap from "gsap/all";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import emailjs, { init } from "emailjs-com";
 
 const SubscribeForm = () => {
+  init("user_WXMrG9H2675m2AFa27nY0");
   const {t}=useTranslation()
-
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      phone_s: "",
+      email_s: "",
+      
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+      .max(30, "Must be 30 characters or less")
+      .min(5, "Must be 5 characters or more")
+      .required("Required"),
+      phone_s: Yup.number()
+      .max(99999999999999, "Must be 15 numbers or less")
+      .min(111111111, "Must be 8 numbers or more")
+      .required("Required"),
+      email_s: Yup.string().email("Invalid email address").required("Required"),
+     
+     
+    }),
+    onSubmit: (values) => {
+      var templateParams = {
+        name: values.name,
+        phone_s:values.phone_s,
+       email_s: values.email_s,
+      };
+      gsap.to(".subscribe-btn", {
+        opacity: 0.5,
+        pointerEvents: "none",
+        duration: 0.1,
+      });
+      emailjs.send("service_ebc0b89", "template_i9k1rca", templateParams).then(
+        (result) => {
+          console.log("this is the response", result.text);
+          alert("Thank you "+ values.name+" for your message 😊");
+          gsap.to(".subscribe-btn", { opacity: 1, pointerEvents: "all" });
+        },
+        (error) => {
+          console.log("this is error", error.text);
+        }
+      );
+    },
+  });
   return (
-    <form className="form-grid">
+    <form className="form-grid" onSubmit={formik.handleSubmit}>
       <div className="form-item">
         <label htmlFor="name">{t("Name")}</label>
         <br />
-        <input required id="name" placeholder="Mark"></input>
+        <input  
+        onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.name} 
+         id="name" placeholder="Mark"></input>
+            {formik.touched.name && formik.errors.name ? (
+            <div className="error-message">{formik.errors.name}</div>
+          ) : null}
       </div>
 
       <div className="form-item">
-        <label htmlFor="name">{t("Phone")}</label>
+        <label htmlFor="phone">{t("Phone")}</label>
         <br />
         <input
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.phone_s}
           type="tel"
-          required
-          pattern="[0-9]{10}"
-          id="contact-number"
+          id="phone_s"
           placeholder="051212***"
         ></input>
+           {formik.touched.phone_s && formik.errors.phone_s ? (
+            <div className="error-message">{formik.errors.phone_s}</div>
+          ) : null}
       </div>
 
       <div className="form-item">
         <label htmlFor="name">{t("Email Address")}</label>
         <br />
         <input
-          required
-          id="email"
+         onChange={formik.handleChange}
+         onBlur={formik.handleBlur}
+         value={formik.values.email_s}
+          id="email_s"
           type="email"
           placeholder="Mark@gmail.com"
         ></input>
+           {formik.touched.email_s && formik.errors.email_s ? (
+            <div className="error-message">{formik.errors.email_s}</div>
+          ) : null}
       </div>
 
       {/* <div className='form-item'>
@@ -40,7 +103,7 @@ const SubscribeForm = () => {
                 <input required type="file" id='file' placeholder='Mark' ></input>
             </div> */}
       <div></div>
-      <button className="form-btn">{t("Submit")}</button>
+      <button className="form-btn subscribe-btn">{t("Submit")}</button>
     </form>
   );
 };
